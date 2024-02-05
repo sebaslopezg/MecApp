6d2e0359bb7b1baccf972dd401da074235df44b2
Sebas <sebaslg96@gmail.com>
2024-02-05 00:40:55 -0500

    agragado cronometro, porcentaje de errores y contador de errores, también se formatea el texto para los ejercicios de manera automatica

--- app.js
+++ app.js
@@ -1,11 +1,55 @@
 let keyboardColor = "#242424"
 let keyActivatedColor = "red"
 let keyNextColor = "blue"
-let selectedArray = leccion_1;
+let selectedArray = formatearTexto(leccion_2)
 let index = 0
 let errores = 0;
+let setError;
+let gameOver = false;
 const letras = document.querySelector("#letras")
+const errorDisplay = document.querySelector("#error_display")
 const limiteCaracteresPantalla = 27
+
+//peuqeño testeo :::
+
+//formatearTexto(leccion_2)
+
+//console.log(formatearTexto(leccion_2))
+
+function formatearTexto(texto){
+    let arrData, arrBase = new Array, respuesta = new Array;
+    let limite, espacio = false
+    arrData = texto.split(" ")
+    limite = (arrData.length + arrData.length)-1
+
+    let index = 0;
+    for(let i = 0; i < limite; i++){
+
+        if(!espacio){
+            arrBase.push(arrData[index])
+            index++
+            espacio = true
+        }else{
+            arrBase.push(" ")
+            espacio = false
+        }
+    }
+
+    let arrayTemporal
+    arrBase.forEach( element =>{
+        if (element == " ") {
+            respuesta.push(" ")
+        }else{
+            arrayTemporal = element.split("")
+            arrayTemporal.forEach( e => {
+                respuesta.push(e)
+            })
+        }
+            
+    })
+
+    return respuesta;
+}
 
 //cuando se selecciona una leccion / mostrar 27 caracteres
 selectedArray.forEach((element, i) => {
@@ -18,25 +62,47 @@ nextKey()
 
 document.addEventListener("keydown", (e)=>{
 
-    for(let i in teclas[e.code]){
-        teclas[e.code][i].style.backgroundColor = keyActivatedColor
-
-        //modificar /revisar
-
-        if(teclas[e.code][i].textContent == selectedArray[index]){
-            updateLetras(index)
-            index++;
-            letras.style.borderColor = "black"
-        }else{
-            letras.style.borderColor = "red"
-            errores++;
-        } 
+    if (!cronometroActivado) {
+        activarCronometro()
     }
+
+    if(!gameOver){
+        for(let i in teclas[e.code]){
+            teclas[e.code][i].style.backgroundColor = keyActivatedColor
+    
+            //modificar /revisar
+    
+            if(teclas[e.code][i].textContent == selectedArray[index]){
+                updateLetras(index)
+                index++;
+                letras.style.borderColor = "black"
+                setError = false;
+            }else{
+                setError = true;
+            } 
+        }
+    
+        if (letras.value == "") {
+            letras.value = "Terminaste!";
+            gameOver = true;
+            clearInterval(cronometroCall)
+            porcentajeDeErrores()
+        }
+    }else{
+
+    }
+
 })
 
-    document.addEventListener("keyup", (e) =>{
-        resetColor(e.code)
-        nextKey()
+document.addEventListener("keyup", (e) =>{
+    resetColor(e.code)
+    nextKey()
+
+    if(setError){
+        letras.style.borderColor = "red"
+        errores++;
+        errorDisplay.innerHTML = "| Errores : " + errores + " |"
+    }
 })
 
 function resetColor(code){
@@ -46,7 +112,6 @@ function resetColor(code){
 }
 
 function updateLetras(){
-    
     let updated = letras.value.substr(1, selectedArray.length)
     letras.value = updated
 }
@@ -60,4 +125,53 @@ function nextKey(){
     }
 }
 
+function error() {
+    letras.style.borderColor = "red"
+    errores++;
+    errorDisplay.innerHTML = "| Errores : " + errores
+}
 
+//cronometro
+let minutos = "00", segundos = "00", milisegundos = "00", cronometroDisplay = document.querySelector("#cronometro")
+let cronometroCall;
+let cronometroActivado = false;
+
+function cronometro(){
+    milisegundos++
+    if (milisegundos < 10) {
+        milisegundos = "0" + milisegundos
+    }
+
+    if (milisegundos > 59) {
+        milisegundos = "00"
+        segundos++
+
+        if (segundos < 10) {
+            segundos = "0" + segundos
+        }
+    }
+
+
+
+    if (segundos > 59) {
+        segundos = "00"
+        minutos++
+
+        if(minutos < 10){
+            minutos = "0" + minutos
+        }
+    }
+    cronometroDisplay.textContent = `${minutos}:${segundos}:${milisegundos}`
+}
+
+function activarCronometro(){
+    cronometroActivado = true;
+    cronometroCall = setInterval(cronometro, 100)
+}
+
+function porcentajeDeErrores(){
+    let porcentaje
+    let displayPorcentaje = document.querySelector("#porcentajeErrores")
+    porcentaje = parseInt(errores * 100 / selectedArray.length) 
+    displayPorcentaje.textContent = `| Porcentaje de errores: ${porcentaje}%` 
+}
--- ejercicios/ejercicios.js
+++ ejercicios/ejercicios.js
@@ -1,3 +1,8 @@
 leccion_0 = ["f", "i", "j", "f", " ", "k"]
 
 leccion_1 = ["j","f","j","j","f"," ","f","f"," ", "f","f","j","f","f"," ","f","f","j"," ","f","f","j","j"," ","j","f","j"," ","j","j"," ","j","f","f","f"," ","j","j","j","f","j"," ", "j","f","f","j"," ","f","f","j","j","j"," ","f","j","f","f"," ","f","f","f"," ","f","f","j","f","f"]
+
+leccion_2 = "dkddk kdkd dkk kkdkd dddd kdd kk ddddd kddkk kkkd kdkk kdddk dd kdd"
+
+// Elementos:  14
+// Espacios: 13     
