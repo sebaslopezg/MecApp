d5cb0d65f6a1e9f43418db62270582d6a3aa9b4b
Sebas <64394400+sebaslopezg@users.noreply.github.com>
2024-02-06 08:04:52 -0500

    Update ejercicios.js
    eliminando comentarios

--- ejercicios/ejercicios.js
+++ ejercicios/ejercicios.js
@@ -3,6 +3,3 @@ leccion_0 = ["f", "i", "j", "f", " ", "k"]
 leccion_1 = "jjjf jjffj fff jjjjf jfjjf fjjj jj jjf fj jjf jfffj jfff jjjff jfff"
 
 leccion_2 = "dkddk kdkd dkk kkdkd dddd kdd kk ddddd kddkk kkkd kdkk kdddk dd kdd"
-
-// Elementos:  14
-// Espacios: 13     
